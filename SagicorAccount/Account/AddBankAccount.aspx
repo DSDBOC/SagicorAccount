4204213396e42b670a8ad727fcf8c4187a83f6c8
Diamond Buchanan <winterdiamon@hotmail.com>
2024-11-25 15:30:01 -0500

    Add BankAccount

--- SagicorAccount/Account/AddBankAccount.aspx
+++ SagicorAccount/Account/AddBankAccount.aspx
@@ -1,3 +1,24 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddBankAccount.aspx.cs" Inherits="SagicorAccount.Account.AddBankAccount" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h2>Add Bank Account</h2>
+    <asp:Label ID="SuccessMessage" runat="server" ForeColor="Green" Visible="false" Text="Bank account added successfully."></asp:Label>
+    <asp:Label ID="ErrorMessage" runat="server" ForeColor="Red" Visible="false"></asp:Label>
+
+    <asp:Panel ID="AddBankAccountPanel" runat="server">
+        <div class="form-group">
+            <label for="AccountType">Account Type:</label>
+            <asp:DropDownList ID="AccountType" runat="server" CssClass="form-control">
+                <asp:ListItem Text="Select Account Type" Value="" />
+                <asp:ListItem Text="Savings" Value="Savings" />
+                <asp:ListItem Text="Checking" Value="Checking" />
+            </asp:DropDownList>
+        </div>
+        <div class="form-group">
+            <label for="InitialBalance">Initial Balance:</label>
+            <asp:TextBox ID="InitialBalance" runat="server" CssClass="form-control" TextMode="Number" Placeholder="Enter initial balance"></asp:TextBox>
+        </div>
+        <asp:Button ID="AddAccountButton" runat="server" Text="Add Account" CssClass="btn btn-primary" OnClick="AddAccountButton_Click" />
+    </asp:Panel>
 </asp:Content>
+
--- SagicorAccount/Account/AddBankAccount.aspx.cs
+++ SagicorAccount/Account/AddBankAccount.aspx.cs
@@ -1,17 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace SagicorAccount.Account
 {
-    public partial class AddBankAccount : System.Web.UI.Page
+    public partial class AddBankAccount : Page
     {
+        private string connectionString = ConfigurationManager.ConnectionStrings["SagicorLifeConnectionString"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                SuccessMessage.Visible = false;
+                ErrorMessage.Visible = false;
+            }
+        }
 
+        protected void AddAccountButton_Click(object sender, EventArgs e)
+        {
+            string accountType = AccountType.SelectedValue;
+            decimal initialBalance;
+            if (string.IsNullOrEmpty(accountType) || !decimal.TryParse(InitialBalance.Text, out initialBalance) || initialBalance < 0)
+            {
+                ErrorMessage.Text = "Please select a valid account type and enter a non-negative balance.";
+                ErrorMessage.Visible = true;
+                return;
+            }
+
+            try
+            {
+                // Fetch logged-in user ID
+                string userID = Session["UserID"] as string;
+                if (string.IsNullOrEmpty(userID))
+                {
+                    ErrorMessage.Text = "Session expired. Please log in again.";
+                    ErrorMessage.Visible = true;
+                    return;
+                }
+
+                // Insert the bank account into the database
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = @"
+                        INSERT INTO BankAccounts (UserID, AccountType, Balance)
+                        VALUES (@UserID, @AccountType, @Balance)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserID", userID);
+                        command.Parameters.AddWithValue("@AccountType", accountType);
+                        command.Parameters.AddWithValue("@Balance", initialBalance);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            SuccessMessage.Text = "Bank account added successfully.";
+                            SuccessMessage.Visible = true;
+                        }
+                        else
+                        {
+                            ErrorMessage.Text = "Failed to add the bank account. Please try again.";
+                            ErrorMessage.Visible = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage.Text = "An error occurred: " + ex.Message;
+                ErrorMessage.Visible = true;
+            }
         }
     }
 }
